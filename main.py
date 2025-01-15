5a806099cdadc142378a37cdd985a2bfa757d340
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-15 18:58:38 +0300

    fix ensemble arg

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 from copy import deepcopy
 from pathlib import Path
+from typing import Literal
 
 import dspy
 import pandas as pd
@@ -81,6 +82,7 @@ def get_predict_cls(technique):
         return dspy.ChainOfThought
     elif technique == "ccot":
         from bellem.dspy.predict.ccot import ConciseChainOfThought
+
         return ConciseChainOfThought
     elif technique == "cte":
         from bellem.dspy.predict.cte import ConnectTheEntities
@@ -118,7 +120,7 @@ def preprocess_result(result):
 def make_results_dataframe(results):
     dataf = pd.json_normalize([preprocess_result(result) for result in results])
     dataf["n_hops"] = dataf["question_decomposition"].apply(len)
-    dataf['predicted_answer'] = dataf['predicted_answer'].fillna("No Answer")
+    dataf["predicted_answer"] = dataf["predicted_answer"].fillna("No Answer")
     return compute_scores_dataframe(dataf)
 
 
@@ -126,13 +128,17 @@ def make_results_dataframe(results):
 def train_main(
     dataset_path: str = typer.Option(..., help="Path to the dataset"),
     dataset_name: str = typer.Option(..., help="Name of the dataset"),
-    dataset_split: str = typer.Option(..., help="Dataset split to use (e.g., 'train', 'validation')"),
+    dataset_split: str = typer.Option(
+        ..., help="Dataset split to use (e.g., 'train', 'validation')"
+    ),
     model: str = typer.Option(..., help="Name of the model to use"),
     temperature: float = typer.Option(..., help="Temperature parameter for the model"),
     technique: str = typer.Option(..., help="Prompting technique to use"),
-    load_from: str = typer.Option(default="UNSET", help="Path to a saved model to load"),
+    load_from: str = typer.Option(
+        default="UNSET", help="Path to a saved model to load"
+    ),
     optimizer_path: Path = typer.Option(..., help="Path to the optimizer config"),
-    ensemble: bool = typer.Option(False, help="Whether to use an ensemble of models"),
+    ensemble: Literal["yes", "no"] = typer.Option( "no", help="Whether to use an ensemble of models"),
     out: Path = typer.Option(..., help="Output file for trained program"),
 ):
     out.parent.mkdir(parents=True, exist_ok=True)
@@ -158,15 +164,17 @@ def train_main(
     if optimizer_config:
         optimizer = make_optimizer(optimizer_config)
         compile_params = optimizer_config.get("compile_params", {})
-        trained_program = optimizer.compile(program, trainset=examples, **compile_params)
+        trained_program = optimizer.compile(
+            program, trainset=examples, **compile_params
+        )
     else:
         trained_program = program
-    
-    if ensemble:
+
+    if ensemble == "yes":
         ensemble_optimizer = Ensemble(reduce_fn=dspy.majority)
         candidate_programs = [x[-1] for x in trained_program.candidate_programs]
         trained_program = ensemble_optimizer.compile(candidate_programs)
-    
+
     # Save the trained program
     trained_program.save(out)
 
@@ -175,11 +183,15 @@ def train_main(
 def evaluate_main(
     dataset_path: str = typer.Option(..., help="Path to the dataset"),
     dataset_name: str = typer.Option(..., help="Name of the dataset"),
-    dataset_split: str = typer.Option(..., help="Dataset split to use (e.g., 'train', 'validation')"),
+    dataset_split: str = typer.Option(
+        ..., help="Dataset split to use (e.g., 'train', 'validation')"
+    ),
     model: str = typer.Option(..., help="Name of the model to use"),
     temperature: float = typer.Option(..., help="Temperature parameter for the model"),
     technique: str = typer.Option(..., help="Prompting technique to use"),
-    load_from: str = typer.Option(default="UNSET", help="Path to a saved model to load"),
+    load_from: str = typer.Option(
+        default="UNSET", help="Path to a saved model to load"
+    ),
     out: Path = typer.Option(..., help="Output directory for generated results"),
 ):
     out.mkdir(parents=True, exist_ok=True)
@@ -215,7 +227,9 @@ def evaluate_main(
     # Save the scores
     scores = aggregate_scores(result_df)
     for n_hops in result_df["n_hops"].unique():
-        scores[f"{n_hops}hops"] = aggregate_scores(result_df[result_df["n_hops"] == n_hops])
+        scores[f"{n_hops}hops"] = aggregate_scores(
+            result_df[result_df["n_hops"] == n_hops]
+        )
 
     with open(out / "scores.json", "w") as f:
         json.dump(scores, f, indent=2)
