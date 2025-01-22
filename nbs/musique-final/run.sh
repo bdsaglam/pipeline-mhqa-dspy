b84f818fe289f96ff7a97d7b6e918dd73ce0c2fc
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-22 23:08:16 +0300

    rename

--- nbs/musique-final/run.sh
+++ nbs/musique-final/run.sh
@@ -1,5 +1,137 @@
 #!/bin/sh
 dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
     -S run=3 \
     -S train.dataset.path='bdsaglam/musique-mini' \
     -S train.dataset.split='train' \
@@ -16,7 +148,235 @@ dvc exp run --queue \
     -S train.dataset.path='bdsaglam/musique-mini' \
     -S train.dataset.split='train' \
     -S train.optimizer='bfsrs-medium' \
-    -S train.ensemble='no' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
     -S evaluation.dataset.path='bdsaglam/musique' \
     -S evaluation.dataset.split='validation' \
     -S qa.model='llama-3-70b-tgi' \
@@ -25,6 +385,78 @@ dvc exp run --queue \
 
 dvc exp run --queue \
     -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.technique='standard'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='ccot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cte'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S train.dataset.path='bdsaglam/musique-mini' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='yes' \
+    -S evaluation.dataset.path='bdsaglam/musique' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.7' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
     -S train.dataset.path='bdsaglam/musique-mini' \
     -S train.dataset.split='train' \
     -S train.optimizer='bfsrs-medium' \
