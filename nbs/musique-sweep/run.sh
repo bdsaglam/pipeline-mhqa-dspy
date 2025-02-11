9a7f157c765b40a2265847ae104bfbb59ddf885b
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-11 15:29:33 +0300

    add musique-sweep exp setup

---
+++ nbs/musique-sweep/run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-sweep' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='miprov2-light' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-sweep' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='miprov2-light' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='agent-simple'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-sweep' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium.json' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/musique-sweep' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-medium.json' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b-tgi' \
+    -S qa.temperature='0.1' \
+    -S qa.technique='agent-simple'
+
