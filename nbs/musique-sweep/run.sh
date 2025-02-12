245a7cc42b8851aecf2c7bdff21b15eecd349043
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-12 21:16:17 +0300

    setup exps

--- nbs/musique-sweep/run.sh
+++ nbs/musique-sweep/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 dvc exp run --queue \
     -S run=1 \
     -S train.dataset.path='bdsaglam/musique-sweep' \
@@ -7,11 +8,12 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S evaluation.dataset.path='bdsaglam/musique-sweep' \
     -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.1' \
+    -S run='1' \
     -S retrieve.model_type='golden' \
     -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-decompose'
+    -S qa.technique='cot'
 
 dvc exp run --queue \
     -S run=1 \
@@ -21,38 +23,11 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S evaluation.dataset.path='bdsaglam/musique-sweep' \
     -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.1' \
+    -S run='1' \
     -S retrieve.model_type='t5' \
     -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-simple'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-simple'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='noop' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='t5' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
     -S qa.technique='multihop-decompose'
 
 dvc exp run --queue \
@@ -63,66 +38,11 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S evaluation.dataset.path='bdsaglam/musique-sweep' \
     -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
+    -S qa.model='llama-3.3-70b' \
     -S qa.temperature='0.1' \
-    -S qa.technique='multihop-decompose'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-decompose'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
+    -S run='1' \
     -S retrieve.model_type='t5' \
     -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-decompose'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='noop' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='agent-simple'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='t5' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
     -S qa.technique='agent-simple'
 
 dvc exp run --queue \
@@ -133,23 +53,10 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S evaluation.dataset.path='bdsaglam/musique-sweep' \
     -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.1' \
+    -S run='1' \
     -S retrieve.model_type='t5' \
     -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
     -S qa.technique='agent-decompose'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/musique-sweep' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/musique-sweep' \
-    -S evaluation.dataset.split='validation' \
-    -S retrieve.model_type='t5' \
-    -S retrieve.top_k='3' \
-    -S qa.model='llama-3.3-70b-tgi' \
-    -S qa.temperature='0.1' \
-    -S qa.technique='multihop-decompose'
 
