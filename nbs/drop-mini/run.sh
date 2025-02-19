d734142340d730e796a172d02da18946c556986f
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-19 19:51:11 +0300

    add new exps

--- nbs/drop-mini/run.sh
+++ nbs/drop-mini/run.sh
@@ -5,80 +5,12 @@ dvc exp run --queue \
     -S train.dataset.path='bdsaglam/drop-musique-mini' \
     -S train.dataset.name='default' \
     -S train.dataset.split='train' \
-    -S train.optimizer='noop' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
-    -S evaluation.dataset.name='default' \
-    -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
-    -S qa.temperature='0.5' \
-    -S run='1' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='1' \
-    -S qa.technique='cot'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/drop-musique-mini' \
-    -S train.dataset.name='default' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
-    -S evaluation.dataset.name='default' \
-    -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
-    -S qa.temperature='0.5' \
-    -S run='1' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='1' \
-    -S qa.technique='cot'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/drop-musique-mini' \
-    -S train.dataset.name='default' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='bfsrs-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
-    -S evaluation.dataset.name='default' \
-    -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
-    -S qa.temperature='0.0' \
-    -S run='1' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='1' \
-    -S qa.technique='cot'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/drop-musique-mini' \
-    -S train.dataset.name='default' \
-    -S train.dataset.split='train' \
     -S train.optimizer='miprov2-light' \
     -S train.ensemble='no' \
     -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
     -S evaluation.dataset.name='default' \
     -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
-    -S qa.temperature='0.5' \
-    -S run='1' \
-    -S retrieve.model_type='golden' \
-    -S retrieve.top_k='1' \
-    -S qa.technique='cot'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.dataset.path='bdsaglam/drop-musique-mini' \
-    -S train.dataset.name='default' \
-    -S train.dataset.split='train' \
-    -S train.optimizer='miprov2-light' \
-    -S train.ensemble='no' \
-    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
-    -S evaluation.dataset.name='default' \
-    -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
+    -S qa.model='llama-3.3-70b' \
     -S qa.temperature='0.0' \
     -S run='1' \
     -S retrieve.model_type='golden' \
@@ -95,8 +27,76 @@ dvc exp run --queue \
     -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
     -S evaluation.dataset.name='default' \
     -S evaluation.dataset.split='validation' \
-    -S qa.model='qwen-2.5-32b' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.5' \
+    -S run='1' \
+    -S retrieve.model_type='golden' \
+    -S retrieve.top_k='1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/drop-musique-mini' \
+    -S train.dataset.name='default' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-light' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
+    -S evaluation.dataset.name='default' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.5' \
+    -S run='1' \
+    -S retrieve.model_type='golden' \
+    -S retrieve.top_k='1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/drop-musique-mini' \
+    -S train.dataset.name='default' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='noop' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
+    -S evaluation.dataset.name='default' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
     -S qa.temperature='0.0' \
+    -S run='1' \
+    -S retrieve.model_type='golden' \
+    -S retrieve.top_k='1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/drop-musique-mini' \
+    -S train.dataset.name='default' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='bfsrs-light' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
+    -S evaluation.dataset.name='default' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.0' \
+    -S run='1' \
+    -S retrieve.model_type='golden' \
+    -S retrieve.top_k='1' \
+    -S qa.technique='cot'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.dataset.path='bdsaglam/drop-musique-mini' \
+    -S train.dataset.name='default' \
+    -S train.dataset.split='train' \
+    -S train.optimizer='miprov2-light' \
+    -S train.ensemble='no' \
+    -S evaluation.dataset.path='bdsaglam/drop-musique-mini' \
+    -S evaluation.dataset.name='default' \
+    -S evaluation.dataset.split='validation' \
+    -S qa.model='llama-3.3-70b' \
+    -S qa.temperature='0.5' \
     -S run='1' \
     -S retrieve.model_type='golden' \
     -S retrieve.top_k='1' \
